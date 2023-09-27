1a8cbbb72750095032e02acd49d7012f6974b454
Hoxuanty <hoxuanty2003qng@gmail.com>
2023-09-27 17:31:38 +0700

    add: function remove

--- src/components/PlayScreen.js
+++ src/components/PlayScreen.js
@@ -1,71 +1,89 @@
 import React, { View, Text, StyleSheet, Pressable } from "react-native";
 import { useState } from 'react';
-
-const  PlayScreen = () => {
+const PlayScreen = () => {
     const [Language, setLanguage] = useState('en');
     const [Index, setIndex] = useState(0);
-
-    const languageDate =[
-        {en:"Hello", vi:"xin chào"},
-        {en:"Mouse", vi:"Chuột"},
-        {en:"Table", vi:"Bàn"}
-    ]
-
-
-    
-    
+    const [languageDate, setLanguageDate] = useState([
+      { en: "Hello", vi: "xin chào" },
+      { en: "Mouse", vi: "Chuột" },
+      { en: "Table", vi: "Bàn" },
+      { en: "computor", vi: "Máy Tính" }
+    ]);
+  
+    const [currentData, setCurrentData] = useState(languageDate[0]);
+  
     const toggleLanguage = () => {
-        if (Language === 'en') {
-            setLanguage('vi');
-        } else {
-            setLanguage('en');
-        }
+      if (Language === 'en') {
+        setLanguage('vi');
+      } else {
+        setLanguage('en');
+      }
     };
+  
     const nextItem = () => {
-        if (Index < languageDate.length - 1) {
-            setIndex(Index + 1);
-        }
+      if (Index < languageDate.length - 1) {
+        setIndex(Index + 1);
+        setCurrentData(languageDate[Index + 1]);
+      }
     };
-    const previousItem = () =>{
-        if (Index){
-            setIndex(Index -1)
+  
+    const previousItem = () => {
+      if (Index > 0) {
+        setIndex(Index - 1);
+        setCurrentData(languageDate[Index - 1]);
+      }
+    };
+  
+    const handleRemove = () => {
+      const newData = [...languageDate];
+      if (newData.length > 0) {
+        newData.splice(Index, 1);
+        setLanguageDate(newData);
+  
+        if (newData.length === 0) {
+          setCurrentData({});
+          setIndex(0);
+        } else if (Index >= newData.length) {
+          setIndex(newData.length - 1);
+          setCurrentData(newData[newData.length - 1]);
+        } else {
+          setCurrentData(newData[Index]);
         }
-    }
-
-    const currentData = languageDate[Index];
-
+      }
+    };
+  
     return (
-        <View style={styles.contents}>
-            <View style={styles.topTitle}>
-              <Text style={styles.Title}>Play({languageDate.length} Cards)</Text>
-            </View>
-            <Pressable style={styles.cardText} onPress={toggleLanguage}>
-                <View style={styles.cardText_middle}>
-                    <Text style={styles.TextChange}>
-                            {currentData[Language]}
-                    </Text>
-                </View>
-            </Pressable>
-            <View style={styles.Derects}>
-                <Pressable style={styles.DerectsBtn} >
-                    <Text style={styles.TextBtn} onPress={previousItem}>Previous</Text>
-                </Pressable>
-                <Pressable style={styles.DerectsBtn} onPress={nextItem}>
-                    <Text style={styles.TextBtn}>Next</Text>
-                </Pressable>
-            </View>
-            <View style={styles.BottomBtn}>
-                <Pressable style={styles.BottomBtn_Tap}>
-                    <Text style={styles.BottomBtn_Text}>Remove from Deck</Text>
-                </Pressable>
-                <Pressable style={styles.BottomBtn_Tap}>
-                    <Text style={styles.BottomBtn_Text}>Reset Deck</Text>
-                </Pressable>
-            </View>
+      <View style={styles.contents}>
+        <View style={styles.topTitle}>
+          <Text style={styles.Title}>Play({languageDate.length} Cards)</Text>
         </View>
+        <Pressable style={styles.cardText} onPress={toggleLanguage}>
+          <View style={styles.cardText_middle}>
+            <Text style={styles.TextChange}>
+              {currentData[Language]}
+            </Text>
+          </View>
+        </Pressable>
+        <View style={styles.Derects}>
+          <Pressable style={styles.DerectsBtn}>
+            <Text style={styles.TextBtn} onPress={previousItem}>Previous</Text>
+          </Pressable>
+          <Pressable style={styles.DerectsBtn} onPress={nextItem}>
+            <Text style={styles.TextBtn}>Next</Text>
+          </Pressable>
+        </View>
+        <View style={styles.BottomBtn}>
+          <Pressable style={styles.BottomBtn_Tap} onPress={handleRemove}>
+            <Text style={styles.BottomBtn_Text}>Remove from Deck</Text>
+          </Pressable>
+          <Pressable style={styles.BottomBtn_Tap}>
+            <Text style={styles.BottomBtn_Text}>Reset Deck</Text>
+          </Pressable>
+        </View>
+      </View>
     );
-}
-
+  }
+  
 const styles = StyleSheet.create({
     contents:{
         flex: 1,
@@ -125,7 +143,7 @@ const styles = StyleSheet.create({
     },
     BottomBtn_Tap:{
         width: '90%',
-        marginVertical: 10,
+        marginVertical: 5,
         paddingVertical: 20,
         backgroundColor: '#FFFFFF',
         alignItems: 'center',
